8c7fbbbe0960d9e245df2c70688ef4f392d79c35
Major_Developer <126969659+MajorDev12@users.noreply.github.com>
2024-06-09 16:28:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp___Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays, lists, collection
+            var names = new List<string> { "major", "simon", "janet", "kevin" };
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+        }
+    }
+}
