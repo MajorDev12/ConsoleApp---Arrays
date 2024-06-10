4554f76987ca2e4f238185925ac69941e1e306ec
Major_Developer <126969659+MajorDev12@users.noreply.github.com>
2024-06-10 13:57:43 +0300

    made a searching algorithm

--- Program.cs
+++ Program.cs
@@ -9,13 +9,40 @@ namespace ConsoleApp___Arrays
         static void Main(string[] args)
         {
             // arrays, lists, collection
-            var names = new List<string> { "major", "simon", "janet", "kevin" };
+            var names = new List<string> { "major", "peter", "Collins", "joyce" };
 
-            foreach (var name in names)
+            while (true)
             {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
+                Console.WriteLine("Please enter the name: ");
+                string searchName = Console.ReadLine();
+                bool found = false;
+
+                // Loop through the list to find the exact match
+                for (int i = 0; i < names.Count; i++)
+                {
+                    if (names[i].Equals(searchName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Print the index if a match is found
+                        Console.WriteLine($"{searchName} is at index {i}");
+                        found = true; 
+                        break; 
+                    }
+                }
+
+                // If the name was found, exit the search loop
+                if (found)
+                {
+                    break;
+                }
+                else
+                {
+                    // Prompt the user to try again if the name wasn't found
+                    Console.WriteLine("Not Found!! Try again.");
+                }
             }
 
+            // Wait for user input before closing the console
+            Console.ReadKey();
         }
     }
 }
